862c7a96db4c66210b8accea885fc9f10cb9f338
Davron Ziyoev <parsifalking0@gmail.com>
2024-06-06 21:47:06 +0500

    Razor Pages-project

---
+++ Domain/Filters/AccountFilter.cs
@@ -0,0 +1,6 @@
+namespace Domain.Filters;
+
+public class AccountFilter : PaginationFilter
+{
+    public decimal? Balance { get; set; }
+}
---
+++ Infrastructure/Services/TransactionService/ITransactionService.cs
@@ -0,0 +1,12 @@
+using Domain.DTOs.TransactionDTO;
+using Domain.Filters;
+using Domain.Responses;
+
+namespace Infrastructure.Services.TransactionService;
+
+public interface ITransactionService
+{
+    Task<PagedResponse<List<GetTransactionsDTO>>> GetTransactionsAsync(TransactionFilter filter);
+    Task<Response<GetTransactionsDTO>> GetTransactionByIdAsync(int transactionId);
+    Task<Response<string>> CreateTransactionAsync(CreateTransactionDTO transaction);
+}
